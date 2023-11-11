76750fd556d9fe68500e17243c92ad42b973b02b
unknown <konankanjulius10@gmail.com>
2023-11-11 03:48:55 +0000

    api get

--- src/components/formulaires/CreateProduct.js
+++ src/components/formulaires/CreateProduct.js
@@ -1,4 +1,55 @@
+import React, {useState, useEffect} from "react";
+
 function CreateProduct() {
+    const [location, setLocation] = useState([]);
+    useEffect(() => {
+      fetch('http://localhost:8000/Emplacement/')
+      .then((response) => response.json())
+      .then((data) =>{
+        console.log(data);
+        setLocation(data);
+      } )
+      .catch((err) => {
+        console.log(err.message);
+      });
+    }, []);
+
+    const locations = location.map( locate=> (
+        <option key={locate.id}>{locate.name}</option>
+    ))
+    const [status, setStatus] = useState([]);
+    useEffect(() => {
+      fetch('http://localhost:8000/status-produit/')
+      .then((response) => response.json())
+      .then((data) =>{
+        console.log(data);
+        setStatus(data);
+      } )
+      .catch((err) => {
+        console.log(err.message);
+      });
+    }, []);
+
+    const statuts = status.map( state=> (
+        <option key={state.id}>{state.name}</option>
+    ))
+
+    const [categorie, setCategorie] = useState([]);
+    useEffect(() => {
+      fetch('http://localhost:8000/categories-produits/')
+      .then((response) => response.json())
+      .then((data) =>{
+        console.log(data);
+        setCategorie(data);
+      } )
+      .catch((err) => {
+        console.log(err.message);
+      });
+    }, []);
+
+    const categories = categorie.map( cat=> (
+        <option key={cat.id}>{cat.name}</option>
+    ))
     return(
         <div>
         <div className="tab-content grid grid-rows-auto grid-rows-1 grid-rows-auto mt-72">
@@ -9,16 +60,19 @@ function CreateProduct() {
                     </div>
                     <div className="">
                         <select className="m-5 p-2 w-72 rounded-lg outline-0 text-neutral-600" name="categories">
-                                <option selected>catégorie</option>
+                                <option value="Catégorie">catégorie</option>
+                                {categories}
                         </select>
                         <select className="m-5 p-2 w-72 rounded-lg outline-0 text-neutral-600" name="status">
-                                <option selected>Status</option>
+                                <option value="Status">Status</option>
+                                {statuts}
                         </select> 
                     </div>
                     
                     <div className="">
                         <select className="m-5 p-2 w-72 rounded-lg outline-0 text-neutral-600" name="location">
-                            <option selected>Emplacement</option>
+                            <option value='Emplacement'>Emplacement</option>
+                            {locations}
                         </select> 
                         <input className="m-5 p-2 w-72 rounded-lg outline-0 text-neutral-600" type="number" name="quantity" placeholder="quantité (Exp: 10)"/>
                     </div>
--- src/components/formulaires/CreateUser.js
+++ src/components/formulaires/CreateUser.js
@@ -1,54 +1,74 @@
-// import '../../style/forms.css'
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react";
 
 function CreateUser() {
-    const [direction, setDirection] = useState([]);
-    useEffect(() => {
-      fetch('http://127.0.0.1:8000/direction/')
+  const [directions, setDirections] = useState([]);
+  const [selectedDirection, setSelectedDirection] = useState("");
+  const [managers, setManagers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/direction/")
       .then((response) => response.json())
-      .then((data) =>{
-        console.log(data);
-        setDirection(data);
-      } )
+      .then((data) => {
+        setDirections(data);
+      })
       .catch((err) => {
         console.log(err.message);
       });
-    }, []);
+  }, []);
 
-    const managments = direction.map(managment =>(
-            <option key={managment.id}>{managment.direction}</option>
-    ))
+  useEffect(() => {
+    // Filtrer les managers pour la direction sélectionnée
+    const selectedDirectionObject = directions.find(
+      (direction) => direction.direction.toUpperCase() === selectedDirection
+    );
 
+    if (selectedDirectionObject) {
+      setManagers([selectedDirectionObject.manager]);
+    } else {
+      setManagers([]);
+    }
+  }, [selectedDirection, directions]);
 
-    return(
-        
-        <div className=''>
-            <div className="tab-content grid grid-rows-auto grid-rows-1 grid-rows-auto mt-72">
-                <div className="content bg-neutral-300 rounded-md shadow-md ml-64 p-14 ">
-                    <form className="" method="POST">
-                        <div className="">
-                            <input className="m-5 p-2 w-72 rounded-lg outline-0 text-neutral-600" type="text" name="username" placeholder="Nom d'utilisateur"/>
-                            <input className="m-5 p-2 w-72 rounded-lg outline-0 text-neutral-600" type="text" name="name" placeholder="Nom et Prénoms"/>
-                        </div>
-                        <div className="">
-                            <input className="m-5 p-2 w-72 rounded-lg outline-0 text-neutral-600" type="text" name="email" placeholder="email@mail.log"/>
-                            <select className="m-5 p-2 w-72 rounded-lg outline-0 text-neutral-600" name="direction">
-                                <option selected>Selectionner</option>
-                                {managments}
-                            </select>
-                        </div>
-                        <div className="">
-                            <input className="m-5 p-2 w-72 rounded-lg outline-0 text-neutral-600" type="text" name="numero" placeholder="Numéro de téléphone"/>
-                            <input className="m-5 p-2 w-72 rounded-lg outline-0 text-neutral-600" type="password" name="password" placeholder="Mot de passe"/>
-                        </div>
-                        <div className="flex justify-center">
-                            <button className="m-5 mb-0 p-2 w-72 bg-neutral-400 rounded-lg text-center text-white hover:bg-neutral-500"  >ENREGISTRER</button>
-                        </div>
-                    </form>
-                </div>
+  const handleDirectionChange = (event) => {
+    setSelectedDirection(event.target.value.toUpperCase());
+  };
+
+  const directionOptions = directions.map((direction) => (
+    <option key={direction.id}>{direction.direction.toUpperCase()}</option>
+  ));
+
+  return (
+    <div className="">
+      <div className="tab-content grid grid-rows-auto grid-rows-1 grid-rows-auto mt-72">
+        <div className="content bg-neutral-300 rounded-md shadow-md ml-64 p-14 ">
+          <form className="" method="POST">
+            <div className="">
+              <input className="m-5 p-2 w-72 rounded-lg outline-0 text-neutral-600" type="text" name="username" placeholder="Nom d'utilisateur"/>
+              <input className="m-5 p-2 w-72 rounded-lg outline-0 text-neutral-600" type="text" name="name"  placeholder="Nom et Prénoms"/>
             </div>
+            <div className="">
+              <input className="m-5 p-2 w-72 rounded-lg outline-0 text-neutral-600" type="text" name="email" placeholder="email@mail.log"/>
+              <input className="m-5 p-2 w-72 rounded-lg outline-0 text-neutral-600" type="text" name="numero" placeholder="Numéro de téléphone"/>
+            </div>
+            <div className="">
+              <select className="m-5 p-2 w-72 rounded-lg outline-0 text-neutral-600" name="direction" onChange={handleDirectionChange} >
+                <option value='Direction'>Direction</option> 
+                {directionOptions}
+              </select>
+              <input className="m-5 p-2 w-72 rounded-lg outline-0 bg-neutral-100 text-neutral-600" placeholder="manager" value={managers.join(", ")} readOnly disabled/>
+            </div>
+            <div>
+              <input className="m-5 p-2 w-11/12 rounded-lg outline-0 text-neutral-600" type="password" name="password" placeholder="Mot de passe"/>
+            </div>
+            <div className="flex justify-center">
+              <button className="m-5 mb-0 p-2 w-72 bg-neutral-400 rounded-lg text-center text-white hover:bg-neutral-500"> ENREGISTRER
+              </button>
+            </div>
+          </form>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default CreateUser
+export default CreateUser;
