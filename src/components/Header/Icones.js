37bc611297f1e66cf9be1b94c1646a94f8d471d1
unknown <konankanjulius10@gmail.com>
2023-11-22 11:38:15 +0000

    icone add

--- src/components/Header/Icones.js
+++ src/components/Header/Icones.js
@@ -3,10 +3,18 @@ import accountIcone from "../../assets/icone/user.png";
 import { BiLogOut } from 'react-icons/bi';
 // import notifIcone from "../../assets/icone/cloche-de-notification.png";
 // import DropDownIcon from "./DropDownIcon";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../../style/dropMenu.css"
 function Icones() {
     const [menu, setMenu] = useState(false);
+
+    useEffect(() => {
+        let handler = ()=> {
+            setMenu(false)
+        }
+
+        document.addEventListener('mousedown', handler)
+    })
 
     return(
         <div className="flex justify-end items-center h-full">
@@ -14,7 +22,7 @@ function Icones() {
             <img src={accountIcone} alt="comptes" className="w-10 pr-3 mr-5 cursor-pointer" onClick={() => {setMenu(!menu)} }/>
             <div className={`dropdown-menu  list-none text-center ${menu? 'active' : 'inactive'}`}>
                 <h3 className="font-bold pb-3">Menu user</h3>
-                <li className="border-t pt-3 flex"><BiLogOut className="mt-1 mr-3 "/>Déconnexion</li>
+                <li className="border-t pt-3 flex cursor-pointer"><BiLogOut className="mt-1 mr-3 "/>Déconnexion</li>
             </div>
         </div>
     )
