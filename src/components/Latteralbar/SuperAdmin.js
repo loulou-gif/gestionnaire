15c6b4fb14fb63d63ca85bf9cd3c33b0d5844424
unknown <konankanjulius10@gmail.com>
2023-11-25 13:10:40 +0000

    color modifs

--- src/components/Latteralbar/SuperAdmin.js
+++ src/components/Latteralbar/SuperAdmin.js
@@ -24,7 +24,7 @@ function SuperAdmin() {
             <div className=" flex justify-normal">
                 <div className="">
                     
-                    <div className="latteral w-72  h-screen bg-orange-400">
+                    <div className="latteral w-72  h-screen bg-blue-900">
                         <nav className=" ">
                             
                             <div className="text-white font-bold text-xl">
@@ -32,48 +32,48 @@ function SuperAdmin() {
                                 <ul className="flex flex-col"> 
                                 
                                     <Link to ="/accueil" id="Gest"  >
-                                        <li className='flex select pt-5 pb-3 pl-5 hover:border-r cursor-pointer  hover:border-black  hover:bg-orange-200'>
+                                        <li className='flex select pt-5 pb-3 pl-5 hover:border-r cursor-pointer  hover:border-black  hover:bg-orange-500'>
                                          <MdManageAccounts className="text-2xl mr-3 " />   Gestion des utilisateurs 
                                         </li>
                                     </Link>   
                                     
                                     <Link to ="/historique" id="Hist" className="w-72" >
-                                        <li className='flex   select pt-5 pb-3 pl-5 hover:border-r cursor-pointer  hover:border-black  hover:bg-orange-200'>
+                                        <li className='flex   select pt-5 pb-3 pl-5 hover:border-r cursor-pointer  hover:border-black  hover:bg-orange-500'>
                                          < TbHistoryToggle className="text-2xl mr-3 "/>   Historiques 
                                         </li>
                                     </Link>   
                                     <Link to ="/stock" id="Stoc"  >
-                                        <li className='flex  select pt-5 pb-3 pl-5 hover:border-r cursor-pointer  hover:border-black  hover:bg-orange-200'>
+                                        <li className='flex  select pt-5 pb-3 pl-5 hover:border-r cursor-pointer  hover:border-black  hover:bg-orange-500'>
                                          <MdOutlineLocalGroceryStore className="text-2xl mr-3 " />    Stocks 
                                         </li>
                                     </Link>   
                                     <li >
-                                        <div className='flex select pt-5 pb-3 pl-5  cursor-pointer hover:border-r hover:border-black  hover:bg-orange-200'onClick={() => setDrop(!drop)}>
+                                        <div className='flex select pt-5 pb-3 pl-5  cursor-pointer hover:border-r hover:border-black  hover:bg-orange-500'onClick={() => setDrop(!drop)}>
                                          <CiSettings className="text-2xl mr-3 "/>     <h3>Paramètre</h3> 
                                         </div>
                                         <div className={`drop-menu transition duration-800 ease-out ${drop? 'active ' : 'inactive ' } `}>
                                             <ul>
 
                                                 <Link to ="/parametre" id="para"  >
-                                                    <li className='flex select pt-5 pb-3 pl-5 w-72 hover:border-r cursor-pointer  hover:border-black  hover:bg-orange-200'>
+                                                    <li className='flex select pt-5 pb-3 pl-5 w-72 hover:border-r cursor-pointer  hover:border-black  hover:bg-orange-500'>
                                                     <RiListSettingsFill className="text-xl mr-3 ml-6"/>    Générale 
                                                     </li>
                                                 </Link>
 
                                                 <Link to ="/status" id="stat"  >
-                                                    <li className='flex select pt-5 pb-3 pl-5 w-72 hover:border-r cursor-pointer  hover:border-black  hover:bg-orange-200'>
+                                                    <li className='flex select pt-5 pb-3 pl-5 w-72 hover:border-r cursor-pointer  hover:border-black  hover:bg-orange-500'>
                                                     <GrPowerCycle className="text-xl mr-3 ml-6"/>     Status 
                                                     </li>
                                                 </Link>
                                                 
                                                 <Link to ="/local" id="local" className="h-full w-full "  > 
-                                                    <li className='flex select pt-5 pb-3 pl-5 w-72 hover:border-r cursor-pointer  hover:border-black  hover:bg-orange-200'>
+                                                    <li className='flex select pt-5 pb-3 pl-5 w-72 hover:border-r cursor-pointer  hover:border-black  hover:bg-orange-500'>
                                                      <MdMyLocation className="text-xl mr-3 ml-6 " />    Local 
                                                     </li>
                                                 </Link>
 
                                                 <Link to ="/categories" id="cate"   >
-                                                    <li className='flex select pt-5 pb-3 pl-5 w-72 hover:border-r cursor-pointer  hover:border-black  hover:bg-orange-200'>
+                                                    <li className='flex select pt-5 pb-3 pl-5 w-72 hover:border-r cursor-pointer  hover:border-black  hover:bg-orange-500'>
                                                     <MdCategory className="text-xl mr-3 ml-6" />     Categories 
                                                     </li>
                                                 </Link>
