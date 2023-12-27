db1755400bc5195388182fe149f68d1d3fb28c1c
unknown <konankanjulius10@gmail.com>
2023-12-27 16:30:05 +0000

    API delete

--- src/components/dataTable/CatList.js
+++ src/components/dataTable/CatList.js
@@ -8,6 +8,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import { Dialog, DialogActions, DialogContentText, DialogTitle, DialogContent } from '@mui/material';
+import axios from 'axios';
 
 function CatList() {
   const [product, setProduct] = useState([]);
@@ -24,6 +25,12 @@ function CatList() {
   }
 
   const handleClose = () => {
+    setOpen(false);
+  }
+
+  const handleDelete = (id) => {
+    axios.delete(`http://localhost:8000/categories-produits/${id}`)
+    setProduct((product) => product.filter((product) => product.id !== id))
     setOpen(false);
   }
 
@@ -112,7 +119,7 @@ function CatList() {
                             </DialogContentText>
                           </DialogContent>
                           <DialogActions className='text-center'>
-                            <button className='w-20 h-10 bg-red-500 rounded-xl duration-200 hover:bg-red-700 hover:duration-300 text-white' onClick={handleClose}> Oui</button>
+                            <button className='w-20 h-10 bg-red-500 rounded-xl duration-200 hover:bg-red-700 hover:duration-300 text-white' onClick={() => handleDelete(row.id)}> Oui</button>
                             <button className='w-20 h-10 bg-green-500 rounded-xl duration-200 hover:bg-green-700 hover:duration-300 text-white' onClick={handleClose}> Non</button>
                           </DialogActions>
                         </Dialog>
